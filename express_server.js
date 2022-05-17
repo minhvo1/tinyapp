32c470861411ecfb31df4803c31820354fcce11c
minhvo1 <minhvo199@gmail.com>
2022-05-17 03:20:39 +0000

    added redirection to long URL page upon clicking short URL

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//Generate random 6-characters string
 function generateRandomString() {
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const length = 6;
@@ -16,6 +17,7 @@ function generateRandomString() {
 
 app.set('view engine', 'ejs');
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -23,30 +25,30 @@ const urlDatabase = {
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Landing page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
+// Page for all current URLs in data base
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL; // Set the long URL input from form to the corresponding short URL generate in database
+  res.redirect(`/urls/${shortURL}`);  // Redirect to /urls/:shortURL
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL); // redirect to long URL page
 });
 
 app.get("/urls/:shortURL", (req, res) => {
