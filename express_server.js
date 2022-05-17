1cf32649548f1c18d97a9f0452731ce1b4fa6cab
minhvo1 <minhvo199@gmail.com>
2022-05-17 23:55:37 +0000

    implemented username cookies and login functionality

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser")
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -24,6 +25,13 @@ const urlDatabase = {
 };
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect('/urls');
+})
 
 // Landing page
 app.get("/", (req, res) => {
@@ -32,7 +40,8 @@ app.get("/", (req, res) => {
 
 // Page for all current URLs in data base
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"],urls: urlDatabase };
+  console.log(templateVars)
   res.render("urls_index", templateVars);
 });
 
@@ -43,7 +52,8 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -53,13 +63,12 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.post("/u/:shortURL", (req, res) => {
   const newLongURL = req.body.longURL;
-  console.log(req.params);
   urlDatabase[req.params.shortURL] = newLongURL;
   res.redirect("/urls");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
